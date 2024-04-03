9929deade63331f20752350e974ddb48d5657be4
Manish Sharma <manish.sharma@ndlm.co.in>
2024-04-03 12:33:52 +0530

    template created.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports = {
     mode: 'development',
@@ -6,13 +7,16 @@ module.exports = {
         filename: "main.[contenthash].js",
         path: path.resolve(__dirname, "dist")
     },
+    plugins: [new HtmlWebpackPlugin({
+        template : "./src/template.html"
+    })],
     module: {
         rules: [
             {
                 test: /\.scss$/,
                 use: ["style-loader", // Step 3 : Injects styles into dom
-                 "css-loader",    // Step 2: Turns css into commonjs
-                  'sass-loader'  // Step 1: Turns sass to css
+                    "css-loader",    // Step 2: Turns css into commonjs
+                    'sass-loader'  // Step 1: Turns sass to css
                 ],
             }
         ]
